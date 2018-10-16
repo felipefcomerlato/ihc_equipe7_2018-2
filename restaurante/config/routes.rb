1df916a975315032086f9e8228938c7a86092bd1
felipefcomerlato <felipef1909@gmail.com>
2018-10-16 14:13:53 -0300

    New route to fix "add order"

--- restaurante/config/routes.rb
+++ restaurante/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get '/menu', to: "items#menu"
   match 'order_item', to: 'items#order_item', via: [:get, :post]
+  post 'add_order', to: 'orders#add_order'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
